c8f53b2fee63e2a16c9aa6d74a782931fb01a9d3
Alexander Morozov <etesial@gmail.com>
2011-12-19 13:57:48 +0400

    Some datatypes modifications

--- Emulator.hs
+++ Emulator.hs
@@ -2,26 +2,33 @@ import Data.Array.MArray
 import Data.Array.Diff
 import Text.Printf
 
-data    Card   = I | Zero | Succ deriving (Show)
+data    Field  = Value Int | Card Card deriving (Show)
 newtype Health = Health Int deriving (Show)
-data    Slot   = Slot Health [Card] deriving (Show)
+data    Slot   = Slot Health Field deriving (Show)
 newtype HBoard = HBoard (DiffArray Int Slot) deriving (Show)
 
-makeHBoard = HBoard $ listArray (0, 255) (repeat $ Slot (Health 10000) [])
+data    Card   = I
+               | Zero
+               | Succ Field
+               | S Field Field Field
+               deriving (Show)
+
+
+
+makeHBoard = HBoard $ listArray (0, 255) (repeat $ Slot (Health 10000) (Card I))
 
 printHBoard :: HBoard -> IO ()
 printHBoard (HBoard slots) = do
     let changed = filter hasChanged (assocs slots)
     mapM_ (putStrLn . format) changed 
     where
-        hasChanged (i, Slot (Health 10000) []) = False
+        hasChanged (i, Slot (Health 10000) (Card I)) = False
         hasChanged _ = True
-        format (i, Slot (Health h) cards) = printf "%d %d %s" i h (show cards)
+        format (i, Slot (Health h) f) = printf "%d %d %s" i h (show f)
 
 main = do
-    -- I hope that compiler won't optimise two instances into one 
     let HBoard a = makeHBoard
-    let a' = a // [(1, Slot (Health 100) []), (4, Slot (Health 200) [Zero])]
+    let a' = a // [(1, Slot (Health 100) (Card Zero)), (4, Slot (Health 200) (Card $ Succ))]
     printHBoard $ HBoard a'
     --putStrLn $ show b
 
