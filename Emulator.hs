e6ddff41d599c199fae5a379f2b901269d1d15f1
Alexander Morozov <etesial@gmail.com>
2011-12-26 03:21:53 +0400

    printing format

--- Emulator.hs
+++ Emulator.hs
@@ -8,21 +8,23 @@ data    Field  = Value Int | Function Card [Field]
 newtype Health = Health Int deriving (Show)
 data    Slot   = Slot Health Field
 newtype HBoard = HBoard (DiffArray Int Slot)
-
+data    LTG    = LTG {opp :: HBoard, prop :: HBoard}
 
 cI = Card "I" 1 f
     where f [x] = id x
 
-cZero = Card "Zero" 0 f
+cZero = Card "zero" 0 f
     where f [] = Value 0
 
-cSucc = Card "Succ" 1 f
+cSucc = Card "succ" 1 f
     where f [Value x] = Value (x+1)
 
 cK = Card "K" 2 f
     where f [x, y] = x
 
-makeHBoard = HBoard $ listArray (0, 255) (repeat $ Slot (Health 10000) (Function cI []))
+
+
+createHBoard = HBoard $ listArray (0, 255) (repeat $ Slot (Health 10000) (Function cI []))
 
 printHBoard :: HBoard -> IO ()
 printHBoard (HBoard slots) = do
@@ -31,14 +33,17 @@ printHBoard (HBoard slots) = do
     where
         hasChanged (i, Slot (Health 10000) (Function cI [])) = False
         hasChanged _ = True
-        format (i, Slot (Health h) f) = printf "%d %d %s" i h (formatField f)
+
+        format (i, Slot (Health h) f) = printf "%d={%d,%s}" i h (formatField f)
+
         formatField (Value v) = show v
+        formatField (Function c []) = cardName c
         formatField (Function c args) = (cardName c) ++
                 "(" ++ (intercalate ", " $ map formatField args) ++ ")"
 
 
 main = do
-    let HBoard a = makeHBoard
+    let HBoard a = createHBoard
     let a' = a // [(1, Slot (Health 100) (Function cZero [])), (4, Slot (Health 10000) (Value 0))]
     printHBoard $ HBoard a'
     --putStrLn $ show b
